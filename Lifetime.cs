23658224fd7b5d8abade462fbb219c7f6f6d2574
士雄 徐 <qazws851228@gmail.com>
2024-11-03 10:29:38 +0800

    加入專案檔案。

---
+++ Lifetime.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IoC_Container
+{
+    internal enum Lifetime
+    {
+        Singleton,
+        Transient
+    }
+}
